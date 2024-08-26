ef0d14bdb7492ffb3cd0be1ddceee7f75a5062a7
Alex <96999944+Jopepka@users.noreply.github.com>
2024-08-26 09:32:00 +0300

    add docker and swagger (#3)

--- src/ITeam/Program.cs
+++ src/ITeam/Program.cs
@@ -1,3 +1,8 @@
+using ITeam.DataAccess;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 namespace ITeam;
 
 public class Program
@@ -5,10 +10,41 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDbContext<ApplicationContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("database")));
+
+        builder.Services.AddControllers();
+
+        RepositoryInjection(builder);
+
+        ServiceInjection(builder);
+
+        builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "ITeam API", Version = "v1" }));
+
+        builder.Services.AddEndpointsApiExplorer();
+
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
         app.Run();
+    }
+
+    private static void RepositoryInjection(WebApplicationBuilder builder)
+    {
+
+    }
+
+    private static void ServiceInjection(WebApplicationBuilder builder)
+    {
+
     }
 }

1917e237cb50762b27628c95330586b18a8ce03c
Alex V <sashavoronenko1@gmail.com>
2024-08-28 01:22:43 +0300

    configurate program

--- src/ITeam/Program.cs
+++ src/ITeam/Program.cs
@@ -1,3 +1,13 @@
+using ITeam.Application.Mapper;
+using ITeam.Application.Services.Modules;
+using ITeam.Application.Services.Products;
+using ITeam.DataAccess;
+using ITeam.DataAccess.Models;
+using ITeam.DataAccess.Repositories.Modules;
+using ITeam.DataAccess.Repositories.Products;
+using ITeam.Presentation.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 namespace ITeam;
 
 public class Program
@@ -5,10 +15,29 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDbContext<ApplicationContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("database")));
+
+
+        builder.Services.AddScoped<IModuleRepository, ModuleRepository>();
+        builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+        builder.Services.AddScoped<IMapper<ModuleDto, ModuleEntity>, ModuleMapper>();
+        builder.Services.AddScoped<IMapper<ProductDto, ProductEntity>, ProductMapper>();
+
+        builder.Services.AddScoped<IModuleService, ModuleService>();
+        builder.Services.AddScoped<IProductService, ProductService>();
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
-
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
         app.Run();
     }
 }
